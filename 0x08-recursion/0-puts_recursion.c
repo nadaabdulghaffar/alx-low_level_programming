ac5b4d0392b40495d62e5d22623d3b792640cb51
nada <nadaabdulghaffar@gmail.com>
2023-04-05 09:24:25 -0700

    0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
 void _puts_recursion(char *s)
 {
 	if (*s)
@@ -10,4 +15,6 @@ void _puts_recursion(char *s)
 	else
 		_putchar('\n');
 }
+
+
 
