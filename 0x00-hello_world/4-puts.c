89714992f28d8a67d23732249c68316e1db6c95d
John Doe <johndoe@example.com>
2023-06-05 16:48:40 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - Entry point
  *
- * Description: A c porgram that prints using put function
- *
- * Return: always 0 (success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,5 @@
-This is reamdme file 
+# ALX School - 0x00-hello_world
+Compiling very simple programs with gcc. Learning the steps of compilation.
+## New commands / functions used:
+``gcc, printf, puts, putchar``
+## Helpful Links
