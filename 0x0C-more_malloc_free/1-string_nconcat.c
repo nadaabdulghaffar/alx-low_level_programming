ee2e0048a5c624ba410888a01891c65516cb85ce
Nada Abdulghaffar <nadaabdulghaffar42@gmail.com>
2023-08-11 06:21:30 +0300

    second commit second task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,35 +1,46 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- *  *char*string_nconcat(char *s1, char *s2, unsigned int n)func conca 2-string
- *   * @s1:first string
- *    * @s2:second string
- *     * @n:number of char of string2
- *      * return:newly allocated space in memory of concatenation string
- *      **/
+ *  * *string_nconcat - concatenates n bytes of a string to another string
+ *   * @s1: string to append to
+ *    * @s2: string to concatenate from
+ *     * @n: number of bytes from s2 to concatenate to s1
+ *      *
+ *       * Return: pointer to the resulting string
+ *        */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-		int len1, len2, counter1, counter2;
-			char *newstring;
-				if (s1 == NULL)
-							s1 = "";
-					if (s2 == NULL)
-								s2 = "";
-						for (len1 = 0; s1[len1] != '\0'; len1++)
-									;
-							for (len2 = 0; s2[len2] != '\0'; len2++)
-										;
-								int new_memory = malloc(len1 + n + 1);
-									if (new_memory == 0)
-												return (NULL);
-										for (counter1 = 0; s1[counter1] != '\0'; counter1++)
-													 newstring[counter1] = s2[counter1];
-											for (counter2 = 0; counter2 > n; counter2++)
-													{
-																newstring[counter1] = s2[counter2];
-																		counter1++;
-																			}
-												newstring[counter1] = '\0';
-													return (newstring);
+		char *s;
+			unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+
+				while (s1 && s1[len1])
+							len1++;
+					while (s2 && s2[len2])
+								len2++;
+
+						if (n < len2)
+									s = malloc(sizeof(char) * (len1 + n + 1));
+							else
+										s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+								if (!s)
+											return (NULL);
+
+									while (i < len1)
+											{
+														s[i] = s1[i];
+																i++;
+																	}
+
+										while (n < len2 && i < (len1 + n))
+													s[i++] = s2[j++];
+
+											while (n >= len2 && i < (len1 + len2))
+														s[i++] = s2[j++];
+
+												s[i] = '\0';
+
+													return (s);
 }
+
