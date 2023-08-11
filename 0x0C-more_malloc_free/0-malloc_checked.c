fdc7d6d0253e4e689588935dd89cd8fbd79f9595
Nada Abdulghaffar <nadaabdulghaffar42@gmail.com>
2023-08-11 15:52:21 +0300

    3rd commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 /**
- *  * *malloc_checked(unsigned int b)-function that allocates memory
- *   * @b: number of bytes
- *    * Return:a pointer to the allocated memory
- *    **/
+ *malloc_checked(unsigned int b)-function that allocates memory
+ *@b:number of bytes
+ * Return:a pointer to the allocated memory
+ **/
 void *malloc_checked(unsigned int b)
 {
-		void *ptr = malloc(b);
+	void *ptr = malloc(b);
 
-			if (ptr == NULL)
-				exit(98);
-				return (ptr);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
 }
