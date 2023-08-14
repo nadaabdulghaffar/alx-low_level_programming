38d0b934dbb96dbb2770bdd12aaa5db93dc7faa5
Nada <nadaabdulghaffar42@gmail.com>
2023-08-14 22:52:17 +0300

    first task struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,20 @@
+
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog- info of dog
+ * @name:dog's name
+ * @age: age 
+ * @owner: dog's owner
+ * 
+ * Description: description of dog struct
+*/
 struct dog
 {
     char *name;
     float age;
     char *owner;
 };
-
 
 #endif DOG_H
