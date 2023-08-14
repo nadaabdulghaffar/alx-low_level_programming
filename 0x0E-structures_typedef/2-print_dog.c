f36c9276514900e2f13ea9116722ad7b20717b9a
Nada <nadaabdulghaffar42@gmail.com>
2023-08-15 00:03:12 +0300

    3rd task 3rd commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,18 +4,20 @@
 #include "dog.h"
 
 /**
- * print_dog-print dog struct info
- * @d: pointer to struct
-*/
+ *print_dog-print dog struct info
+ *@d: pointer to struct
+ */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
+    if (d)
+    {
+		if (d->name == NULL)
+			d->name = "(nil)";
+		if (d->owner == NULL)
+			d->owner = "(nil)";
 
-	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->owner == NULL)
-		d->owner = "(nil)";
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+    }
+
 }
