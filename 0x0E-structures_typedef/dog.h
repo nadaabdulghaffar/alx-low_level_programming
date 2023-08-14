bdae2af497954abb350e6f27305f89ef01ac6d87
Nada <nadaabdulghaffar42@gmail.com>
2023-08-14 22:54:22 +0300

    first task struct 2nd commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,18 +3,18 @@
 #define DOG_H
 
 /**
- * struct dog- info of dog
- * @name:dog's name
- * @age: age 
- * @owner: dog's owner
- * 
- * Description: description of dog struct
-*/
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
-#endif DOG_H
+#endif
