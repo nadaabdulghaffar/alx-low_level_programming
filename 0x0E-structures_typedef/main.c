31449bcd502e91b3de5e2b2a921db2a8b4f3f6cc
Nada <nadaabdulghaffar42@gmail.com>
2023-08-14 23:42:49 +0300

    3rd task 1st commit

---
+++ 0x0E-structures_typedef/main.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+
+int main(){
+      struct dog my_dog;
+
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    print_dog(&my_dog);
+    return (0);
+
+}
