be42c009aebd87368034dc5bf4a12cbdd48bd36e
Nada <nadaabdulghaffar42@gmail.com>
2023-08-14 22:27:42 +0300

    first commit all 3rd time

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main()-print complied file name
- * return: 0
-*/
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("%s\n",__FILE__);
-    return(0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
