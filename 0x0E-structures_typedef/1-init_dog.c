0b66856070c2bd2f60d6b21259e0d3262bc5246d
Nada <nadaabdulghaffar42@gmail.com>
2023-08-14 23:07:32 +0300

    second task first commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog-function init info of dog
+ * @d: pointer to struct
+ * @name:dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * return: void
+**/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+        if (d)
+        {
+            d->name;
+            d->age;
+            d->owner;
+        }
+}
