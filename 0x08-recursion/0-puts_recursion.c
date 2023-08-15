082337df385e5cff3ff03fa6175a0e26cd2027de
Nada <nadaabdulghaffar42@gmail.com>
2023-08-15 16:12:10 +0300

    first task first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _puts_recursion(char *s)- prints a string, followed by a new line.
+ * @s:input to print
+*/
+void _puts_recursion(char *s)
+{
+    if(*s)   
+    {
+        _putchar(*s);
+        _puts_recursion(s+1);
+    }
+    else 
+        _putchar('\n');
+}
