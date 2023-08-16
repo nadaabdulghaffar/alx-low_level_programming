5ac2589b5b3c5c37c42850bf12f01d04f8a8a800
Nada <nadaabdulghaffar42@gmail.com>
2023-08-17 01:22:55 +0300

    first task first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * void print_name(char *name, void (*f)(char *))-prints a name.
+ * @name: string to print
+ * @f:function pointer
+**/
+void print_name(char *name, void (*f)(char *))
+{
+	if (*name && f)
+		f(name);
+}
+
