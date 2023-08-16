9533f13a103431d7bc15d169c5aea5c62b526269
Nada <nadaabdulghaffar42@gmail.com>
2023-08-17 01:38:55 +0300

    first task 3rd commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_POINTER_H
 
 #include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
@@ -9,3 +10,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif 
+
