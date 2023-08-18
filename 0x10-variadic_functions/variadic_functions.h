94cd0b7ec043fa8e402ffa857b06bfbc8b65fa3b
Nada <nadaabdulghaffar42@gmail.com>
2023-08-18 09:34:31 +0300

    first task first commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,9 @@
 #ifndef VARIADIC_FUNCTIONIS_H
 #define VARIADIC_FUNCTIONIS_H
 
+#include <stdio.h>
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
