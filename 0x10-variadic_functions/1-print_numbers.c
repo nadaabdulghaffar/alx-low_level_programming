13228a82bf2899d0b069182b34dc566cc29ea46c
Nada <nadaabdulghaffar42@gmail.com>
2023-08-18 11:02:00 +0300

    second task 9 commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,7 +7,7 @@
  * @separator: is the string to be printed between numbers
  * @n:number of integers passed to the function
  * @...: number of parameters passed to the function
- */
+**/
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 		
-		if (separator != NULL && i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
