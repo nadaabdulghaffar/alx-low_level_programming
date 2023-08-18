f90c101832c5ef8cb000a1eaf9faf31985864a40
Nada <nadaabdulghaffar42@gmail.com>
2023-08-18 10:05:07 +0300

    first task 2nd commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,24 +4,22 @@
 /**sum_them_all()-find sum of all its parameters.
  * @n:number of paramters
  * @...:A variable number of paramters to calculate the sum of.
- * 
- * Return:If n == 0, return 0 
- Otherwise - the sum of all parameters.
-*/
+ * Return:If n == 0, return 0
+ * Otherwise - the sum of all parameters.
+**/
 int sum_them_all(const unsigned int n, ...)
 {
-if (n)
+if (n == 0)
+	return (0);
+else
 {
 	va_list args;
 	int sum = 0;
 	va_start(args, n);
-	for (int i = 0; i < n; i++)
+	for (unsigned int i = 0; i < n; i++)
 		sum += va_arg(args, int);
 	va_end(args);
 	return (sum);
-
 }
-else
-	return (0);
 }
 
