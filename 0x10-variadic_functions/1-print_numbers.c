438d3cae0ce815c6c0f5a7da061a68af0bf07a37
Nada <nadaabdulghaffar42@gmail.com>
2023-08-18 10:50:04 +0300

    second task 4th commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,10 +11,11 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
+	unsigned int i ;
 	
 	va_start(args, n);
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
 		
