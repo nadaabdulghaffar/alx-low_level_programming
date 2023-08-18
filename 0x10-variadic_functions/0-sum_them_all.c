cfe6e30967c5c6e05cc27eb093fb118ac7bd1866
Nada <nadaabdulghaffar42@gmail.com>
2023-08-18 10:09:22 +0300

    first task 2nd commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,17 +9,17 @@
 **/
 int sum_them_all(const unsigned int n, ...)
 {
-if (n == 0)
-	return (0);
-else
-{
 	va_list args;
 	int sum = 0;
-	va_start(args, n);
+	
+	if (n == 0)
+		return (0);
+	va_start(args, n);	
 	for (unsigned int i = 0; i < n; i++)
 		sum += va_arg(args, int);
 	va_end(args);
 	return (sum);
+
 }
-}
+
 
