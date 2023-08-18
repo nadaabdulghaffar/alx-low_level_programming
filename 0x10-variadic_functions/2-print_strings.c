7321b86b8b71ee8c439668b3997a506630425221
Nada <nadaabdulghaffar42@gmail.com>
2023-08-18 16:09:47 +0300

    3rd task second commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,12 +3,12 @@
 #include "variadic_functions.h"
 
 /**
- * ()-functionprints strings, followed by a new line.
+ * print_strings()-function prints strings, followed by a new line.
  * @separator: is string printed between the strings
  * @n:number of strings passed to the function
  * @...:the variabls that passed to the function
- * 
- * Description:If separator is NULL, don’t print it
+ *
+ * description:If separator is NULL, don’t print it
  *             If one of the string is NULL, print (nil) instead
  */
 void print_strings(const char *separator, const unsigned int n, ...)
