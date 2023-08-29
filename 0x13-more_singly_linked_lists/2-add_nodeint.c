93bd60d22524ac6d239a95c6e719b19b191593ca
Nada <nadaabdulghaffar42@gmail.com>
2023-08-29 21:29:14 +0300

    second commit 2nd try

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,15 +12,15 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newnode = malloc(sizeof(listint_t));
-	if (newnode)
-	{
-		newnode->n = n;
-		newnode->next = *head;
-		*head = newnode;
-		return (newnode);
-	}
-	else
+	
+	if (!newnode)
 		return (NULL);
+
+	newnode->n = n;
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
 }
+
 
 
