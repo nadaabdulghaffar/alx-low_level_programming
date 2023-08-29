a44ab2639049a9870a4d512723f9d686e1a515ac
Nada <nadaabdulghaffar42@gmail.com>
2023-08-29 21:09:29 +0300

    first commit 6 try

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,8 +4,9 @@
 /**
  * listint_len() - returns the number of elements in a linked
  * @h: linked list fo type listint_t
- * Returns: the number of elements
-*/
+ * 
+ * Return: the number of elements
+ */
 size_t listint_len(const listint_t *h)
 {
 	size_t counter = 0;
