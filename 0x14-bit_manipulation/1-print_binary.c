da0c18073c2a33681468dc87c816e7817a1980c2
Nada <nadaabdulghaffar42@gmail.com>
2023-09-29 22:07:45 +0300

    bit 2nd task 2nd commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- *print_binary() -prints binary representation of a number.
- *@n:number to convert
- *
- *Return: void
- */
+*print_binary() -prints binary representation of a number.
+*@n:number to convert
+*
+*Return: void
+*/
 void print_binary(unsigned long int n)
 {
+
 	if (n > 1)
 		print_binary(n >> 1);
 
