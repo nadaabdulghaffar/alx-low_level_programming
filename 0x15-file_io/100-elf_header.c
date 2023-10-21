702b0f3f0ab9b2a974432f2143166f0d06fca54d
Nada <nadaabdulghaffar42@gmail.com>
2023-10-21 14:10:54 +0300

    last try

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,10 +1,10 @@
-#include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <elf.h>
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
