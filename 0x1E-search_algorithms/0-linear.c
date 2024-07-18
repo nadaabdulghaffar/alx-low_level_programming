e73ddc89b7ae17703447fe075c202dcc8f505a3e
=Nada <nadaabdulghaffar42@gmail.com>
2024-07-18 21:29:04 +0300

    task 1.4

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,6 +18,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (index = 0; index < size; index++)
 	{
+		printf("Value checked array[%zu] = [%d]\n", index, array[index]);
 		if (array[index] == value)
 			return (index);
 	}
